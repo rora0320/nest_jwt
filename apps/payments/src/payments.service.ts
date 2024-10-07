3875232a5481eb6e6cee2d02769a0fa8bb658ad1
rora0320 <lkhpgrc@gmail.com>
2024-10-07 14:23:46 +0900

    stripe 설치 및 create

--- apps/payments/src/payments.service.ts
+++ apps/payments/src/payments.service.ts
@@ -1,8 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import Stripe from 'stripe';
+import { ConfigService } from '@nestjs/config';
+import { CreateChargeDto } from './dto/create-charge.dto';
 
 @Injectable()
 export class PaymentsService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly configService: ConfigService) {}
+  private readonly stripe = new Stripe(this.configService.get('STRIPE_KEY'), {
+    apiVersion: '2024-09-30.acacia',
+  });
+
+  async createCharge({ card, amount }: CreateChargeDto) {
+    const paymentMethod = await this.stripe.paymentMethods.create({
+      type: 'card',
+      card,
+    });
+
+    return await this.stripe.paymentIntents.create({
+      payment_method: paymentMethod.id,
+      amount: amount * 100,
+      confirm: true,
+      payment_method_types: ['card'],
+      currency: 'usd',
+    });
   }
 }
